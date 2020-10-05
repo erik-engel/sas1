2e223eb714253c022fbec4639f2ab2c720555778
ehenr <erik1500@stud.kea.dk>
2020-10-05 15:33:27 +0200

    Model ship and child classes

--- src/main/java/sas1/project/demo/model/Ship.java
+++ src/main/java/sas1/project/demo/model/Ship.java
@@ -1,39 +1,46 @@
 package sas1.project.demo.model;
 
 public class Ship {
-    int guns;
     int sailors;
     int maxSailors;
+    //guns
+    int rowsOfGuns;
+    int gunsPrRow;
+    int gunsSailorsNeeded;
+    //sails
+    int sailsHP;
     int sails;
     int maxSails;
-    int hull;
-    int maxHull;
+    int sailsSailorsNeeded;
+    //hulls
+    int hullHP;
+    int maxHullHP;
+    //speed
     int speed;
     int maxSpeed;
     int maxSpeedChange;
+    //moves
+    int numberOfTurns;
 
     public Ship() {
     }
 
-    public Ship(int guns, int sailors, int maxSailors, int sails, int maxSails, int hull, int maxHull, int speed, int maxSpeed, int maxSpeedChange) {
-        this.guns = guns;
+    public Ship(int sailors, int maxSailors, int rowsOfGuns, int gunsPrRow, int gunsSailorsNeeded, int sailsHP, int sails, int maxSails, int sailsSailorsNeeded, int hullHP, int maxHullHP, int speed, int maxSpeed, int maxSpeedChange, int numberOfTurns) {
         this.sailors = sailors;
         this.maxSailors = maxSailors;
+        this.rowsOfGuns = rowsOfGuns;
+        this.gunsPrRow = gunsPrRow;
+        this.gunsSailorsNeeded = gunsSailorsNeeded;
+        this.sailsHP = sailsHP;
         this.sails = sails;
         this.maxSails = maxSails;
-        this.hull = hull;
-        this.maxHull = maxHull;
+        this.sailsSailorsNeeded = sailsSailorsNeeded;
+        this.hullHP = hullHP;
+        this.maxHullHP = maxHullHP;
         this.speed = speed;
         this.maxSpeed = maxSpeed;
         this.maxSpeedChange = maxSpeedChange;
-    }
-
-    public int getGuns() {
-        return guns;
-    }
-
-    public void setGuns(int guns) {
-        this.guns = guns;
+        this.numberOfTurns = numberOfTurns;
     }
 
     public int getSailors() {
@@ -52,6 +59,38 @@ public class Ship {
         this.maxSailors = maxSailors;
     }
 
+    public int getRowsOfGuns() {
+        return rowsOfGuns;
+    }
+
+    public void setRowsOfGuns(int rowsOfGuns) {
+        this.rowsOfGuns = rowsOfGuns;
+    }
+
+    public int getGunsPrRow() {
+        return gunsPrRow;
+    }
+
+    public void setGunsPrRow(int gunsPrRow) {
+        this.gunsPrRow = gunsPrRow;
+    }
+
+    public int getGunsSailorsNeeded() {
+        return gunsSailorsNeeded;
+    }
+
+    public void setGunsSailorsNeeded(int gunsSailorsNeeded) {
+        this.gunsSailorsNeeded = gunsSailorsNeeded;
+    }
+
+    public int getSailsHP() {
+        return sailsHP;
+    }
+
+    public void setSailsHP(int sailsHP) {
+        this.sailsHP = sailsHP;
+    }
+
     public int getSails() {
         return sails;
     }
@@ -68,20 +107,28 @@ public class Ship {
         this.maxSails = maxSails;
     }
 
-    public int getHull() {
-        return hull;
+    public int getSailsSailorsNeeded() {
+        return sailsSailorsNeeded;
     }
 
-    public void setHull(int hull) {
-        this.hull = hull;
+    public void setSailsSailorsNeeded(int sailsSailorsNeeded) {
+        this.sailsSailorsNeeded = sailsSailorsNeeded;
     }
 
-    public int getMaxHull() {
-        return maxHull;
+    public int getHullHP() {
+        return hullHP;
     }
 
-    public void setMaxHull(int maxHull) {
-        this.maxHull = maxHull;
+    public void setHullHP(int hullHP) {
+        this.hullHP = hullHP;
+    }
+
+    public int getMaxHullHP() {
+        return maxHullHP;
+    }
+
+    public void setMaxHullHP(int maxHullHP) {
+        this.maxHullHP = maxHullHP;
     }
 
     public int getSpeed() {
@@ -108,19 +155,33 @@ public class Ship {
         this.maxSpeedChange = maxSpeedChange;
     }
 
+    public int getNumberOfTurns() {
+        return numberOfTurns;
+    }
+
+    public void setNumberOfTurns(int numberOfTurns) {
+        this.numberOfTurns = numberOfTurns;
+    }
+
     @Override
     public String toString() {
         return "Ship{" +
-                "guns=" + guns +
-                ", sailors=" + sailors +
+                "sailors=" + sailors +
                 ", maxSailors=" + maxSailors +
+                ", rowsOfGuns=" + rowsOfGuns +
+                ", gunsPrRow=" + gunsPrRow +
+                ", gunsSailorsNeeded=" + gunsSailorsNeeded +
+                ", sailsHP=" + sailsHP +
                 ", sails=" + sails +
                 ", maxSails=" + maxSails +
-                ", hull=" + hull +
-                ", maxHull=" + maxHull +
+                ", sailsSailorsNeeded=" + sailsSailorsNeeded +
+                ", hullHP=" + hullHP +
+                ", maxHullHP=" + maxHullHP +
                 ", speed=" + speed +
                 ", maxSpeed=" + maxSpeed +
                 ", maxSpeedChange=" + maxSpeedChange +
+                ", numberOfTurns=" + numberOfTurns +
                 '}';
     }
 }
+
---
+++ src/main/java/sas1/project/demo/model/shipManAtWar.java
@@ -0,0 +1,31 @@
+package sas1.project.demo.model;
+
+public class shipManAtWar extends Ship {
+    int sailors = 340;
+    int maxSailors = 340;
+    //guns
+    int rowsOfGuns = 3;
+    int gunsPrRow = 28;
+    int gunsSailorsNeeded = 84;
+    //sails
+    int sailsHP = 80;
+    int sails = 24;
+    int maxSails = 24;
+    int sailsSailorsNeeded = 144;
+    //hulls
+    int hullHP = 140;
+    int maxHullHP = 140;
+    //speed
+    int speed = 0;
+    int maxSpeed = 4;
+    int maxSpeedChange = 1;
+    //moves
+    int numberOfTurns = 1;
+
+    public shipManAtWar() {
+    }
+
+
+
+
+}
