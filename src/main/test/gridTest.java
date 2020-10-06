06b1c09b71039c63f4d68b30305be7949efeda9a
Ironfirst <57720071+Ironfirst@users.noreply.github.com>
2020-10-06 08:59:50 +0200

    initial commit på map repository

---
+++ src/main/test/gridTest.java
@@ -0,0 +1,26 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import sas1.project.demo.model.grid;
+
+class gridTest
+
+{
+
+    //System.out.println("Input 1 for a small map"); size 10
+      //  System.out.println("Input 2 for a medium map"); size 20
+        //System.out.println("Input 3 for a large map"); size 30
+        //System.out.println("Input 0 to exit"); error
+
+    grid Grid = new grid();
+
+
+    @Test
+    void gridTest()
+    {
+
+       // assertNotNull(1,);
+}
+
+}
