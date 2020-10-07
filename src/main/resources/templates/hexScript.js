f019fe15c6965da03d231822b356330c48c1a196
Pelle <pelle.park@gmail.com>
2020-10-07 22:36:59 +0200

    html hex

--- src/main/resources/templates/hexScript.js
+++ src/main/resources/templates/hexScript.js
@@ -1,6 +1,6 @@
-
 function createRow(x, y){
-    for (i = 0; i < x; i++){
+    let name;
+    for (let i = 0; i < x; i++){
         name = "row-even"
         if (i % 2 === 0){
             name = "row"
@@ -9,11 +9,10 @@ function createRow(x, y){
         row.classList.add(name);
         const el = document.getElementById("board");
         el.appendChild(row);
-        for (j = 0; j < y; j++){
+        for (let j = 0; j < y; j++){
             const hex = document.createElement("div");
             hex.id = i + "," + j;
-            hex.classList.add("hexagon");
-            hex.classList.add("wave");
+            hex.classList.add("hexagon", "wave");
             hex.addEventListener("click", function (e){
                 if (hex.classList.contains("wave")){
                 setShip(hex.id);
@@ -27,13 +26,35 @@ function createRow(x, y){
 }
 
 function setShip(shipPosition){
-    ship = document.getElementById(shipPosition);
+    let ship = document.getElementById(shipPosition);
     ship.classList.remove("wave");
     ship.classList.add("ship");
 }
 
 function setWave(shipPosition){
-    ship = document.getElementById(shipPosition);
+    let ship = document.getElementById(shipPosition);
     ship.classList.remove("ship");
     ship.classList.add("wave");
+}
+
+function setSailAble(hexList){
+    for (let i = 0; i < hexList.length; i++){
+        hexList[i].classList.add("moveable");
+    }
+}
+
+function removeSailAble(){
+    let sail = document.getElementsByClassName("moveable");
+    while (sail.length > 0){
+        console.log(sail[0].id)
+        sail[0].classList.remove("moveable");
+    }
+}
+
+function sailListTest(){
+    let sailList = [];
+    sailList.push(document.getElementById("1,1"));
+    sailList.push(document.getElementById("2,2"));
+    sailList.push(document.getElementById("3,3"));
+    return sailList;
 }
