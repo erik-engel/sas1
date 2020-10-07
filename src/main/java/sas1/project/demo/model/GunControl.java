97536cae4a138b8a633f43486b048587e95afb58
Mullefyren <57482909+Mullefyren@users.noreply.github.com>
2020-10-07 14:44:42 +0200

    Comments

--- src/main/java/sas1/project/demo/model/GunControl.java
+++ src/main/java/sas1/project/demo/model/GunControl.java
@@ -11,8 +11,9 @@ public class GunControl {
 
     public void setDamage(Ship ship, Ammunition ammo) {
         Random rand = new Random();
+        // int random mellem 1-20 fro at se om der crittes ved 1 & 2 eller over 2 som er almindelig canon damage pr canon.
         int roll = rand.nextInt(20) + 1;
-
+        // switch på ternings roll som bestemmer damage på modstander skib, ammunition skal opdateres så man kan få criteffect fra listen
         switch (roll){
             case 1:
                 this.damage = ammo.getCritEffect(0);
@@ -21,6 +22,7 @@ public class GunControl {
                 this.damage = ammo.getCritEffect(1);
             break;
             default:
+                // dividere med 2 så det kun er canons fra den ene side, da currentGunCapacity tager udgangspunkt i alle canons
                 this.damage = ship.currentGunCapacity() / 2;
             break;
         }
