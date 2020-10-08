5ad580acecf6fc73e85d430348800d2cfdd546d8
Cecilie <cecilie@brandt.net>
2020-10-08 13:10:45 +0200

    to nye klasser

---
+++ src/main/java/sas1/project/demo/model/Game.java
@@ -0,0 +1,8 @@
+package sas1.project.demo.model;
+
+public class Game {
+    private Ocean ocean;
+    private Wind wind;
+    private Round roundP1;
+    private Round roundP2;
+}
---
+++ src/main/java/sas1/project/demo/model/Round.java
@@ -0,0 +1,9 @@
+package sas1.project.demo.model;
+
+import java.util.List;
+
+public class Round {
+    private Ship ship;
+    private List<Hex> movesList;
+    private Hex target;
+}
