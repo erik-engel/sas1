3fd24e64f9f6cc2b6d2fb3c5826eaaf440fe03ca
Ironfirst <57720071+Ironfirst@users.noreply.github.com>
2020-10-08 14:26:47 +0200

    test classe navngivning ændret,
    IllegalArgumentException til oceanX og oceanY er lavet så
    værdierne ikke kan være under 1 ved oprettelse

---
+++ src/main/java/sas1/project/demo/model/Movement.java
@@ -0,0 +1,22 @@
+package sas1.project.demo.model;
+
+public class Movement extends Ship, Speed
+{
+
+    // hvor kan skiber rykke hen
+    // udregne mulige ryk ud fra direction
+
+    //finde ship speed
+    // finde ud af om det muligt at dreje "boolean fra ship skal sættes false eller true
+    // hvis antal mulige ryk er brugt
+
+
+    // gemme temp objects med ryk
+
+
+
+
+
+
+
+}
