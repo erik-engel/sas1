4d2cbe81aa218aaa09a04e657672b8281bcbe63e
Pelle <pelle.park@gmail.com>
2020-10-08 11:34:47 +0200

    html hex

--- src/main/resources/templates/hexScript.js
+++ src/main/resources/templates/hexScript.js
@@ -1,3 +1,6 @@
+var moveAction = false;
+var aimAction = false;
+
 function createRow(x, y){
     let name;
     for (let i = 0; i < x; i++){
@@ -14,14 +17,35 @@ function createRow(x, y){
             hex.id = i + "," + j;
             hex.classList.add("hexagon", "wave");
             hex.addEventListener("click", function (e){
-                if (hex.classList.contains("wave")){
-                setShip(hex.id);
-                } else {
-                    setWave(hex.id);
+                if (moveAction) {
+                    if (hex.classList.contains("moveable")){
+                        removeMoveto();
+                        hex.classList.add("moveto");
+                    }
+                }
+                if (aimAction) {
+                    if (hex.classList.contains("aimable")){
+                        removeAimto();
+                        hex.classList.add("aimto");
+                    }
                 }
             })
             row.appendChild(hex);
         }
+    }
+}
+
+function removeMoveto(){
+    let aim = document.getElementsByClassName("moveto");
+    while (aim.length > 0) {
+        aim[0].classList.remove("moveto");
+    }
+}
+
+function removeAimto(){
+    let aim = document.getElementsByClassName("aimto");
+    while (aim.length > 0) {
+        aim[0].classList.remove("aimto");
     }
 }
 
@@ -43,6 +67,12 @@ function setSailAble(hexList){
     }
 }
 
+function setAimAble(hexList){
+    for (let i = 0; i < hexList.length; i++){
+        hexList[i].classList.add("aimable")
+    }
+}
+
 function removeSailAble(){
     let sail = document.getElementsByClassName("moveable");
     while (sail.length > 0){
@@ -51,10 +81,35 @@ function removeSailAble(){
     }
 }
 
-function sailListTest(){
-    let sailList = [];
-    sailList.push(document.getElementById("1,1"));
-    sailList.push(document.getElementById("2,2"));
-    sailList.push(document.getElementById("3,3"));
-    return sailList;
+function removeAimAble(){
+    let sail = document.getElementsByClassName("aimable");
+    while (sail.length > 0){
+        console.log(sail[0].id)
+        sail[0].classList.remove("aimable");
+    }
 }
+
+function testStart(){
+    moveAction = true;
+    setShip('2,2')
+    let sailList = [];
+    sailList.push(document.getElementById("3,2"));
+    sailList.push(document.getElementById("3,3"));
+    sailList.push(document.getElementById("2,3"));
+    setSailAble(sailList);
+}
+
+function testStart2(){
+    moveAction = false;
+    aimAction = true;
+    removeSailAble()
+    let aimList = [];
+    aimList.push(document.getElementById("2,0"));
+    aimList.push(document.getElementById("3,1"));
+    aimList.push(document.getElementById("4,1"));
+    aimList.push(document.getElementById("0,3"));
+    aimList.push(document.getElementById("1,4"));
+    aimList.push(document.getElementById("2,4"));
+    setAimAble(aimList);
+}
+
